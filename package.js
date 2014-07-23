d9ef1d8efdd8db10c6f3dcc6d1129764b63d99bc
stefanocudini <stefano.cudini@gmail.com>
2014-07-23 22:21:30 +0200

    fix meteor include files

--- package.js
+++ package.js
@@ -3,5 +3,5 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
-	api.add_files('bootstrap-confirm-button.js', 'client');
+	api.add_files('bootstrap-confirm-button.src.js', 'client');
 });
